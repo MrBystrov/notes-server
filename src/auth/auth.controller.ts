f1b29769884a48e83b00d19f1cf842dbdd140431
Sergei <bodri4@yandex.ru>
2025-03-24 09:58:35 +0300

    Update mongo connection

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -23,7 +23,7 @@ export class AuthController {
 
     response.cookie('token', token, {
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: 'None', // Разрешить кросс-доменные запросы
+      sameSite: 'none', // Разрешить кросс-доменные запросы
       secure: true,
     });
 
