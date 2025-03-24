e31cef7ca3c2b32273cd4f0f5b15d3ef34223f68
Sergei <bodri4@yandex.ru>
2025-03-24 11:01:21 +0300

    Update mongo connection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'https://notes-frontend-sci7.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
