3b91d63a8d00fb2eb8d6a1e4053932bda2d4fdba
Sergei <bodri4@yandex.ru>
2025-03-24 08:48:11 +0300

    Update mongo connection

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,6 +22,7 @@ export class AuthController {
     );
 
     response.cookie('token', token, {
+      httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
       secure: true,
       sameSite: 'none',
